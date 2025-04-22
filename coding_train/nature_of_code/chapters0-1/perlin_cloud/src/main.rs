2f9c24e134f46d076867a53dd54d366f07e7be1d
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-04-22 16:46:57 -0400

    Perlin Cloud completed.

---
+++ coding_train/nature_of_code/chapters0-1/perlin_cloud/src/main.rs
@@ -0,0 +1,46 @@
+use macroquad::prelude::*;
+use noise::{ Fbm, Perlin, NoiseFn };
+
+#[macroquad::main("Perlin Cloud")]
+async fn main() {
+    let width = screen_width();
+    let height = screen_height();
+
+    let r = rand::gen_range(0,u32::MAX);
+    let noise = Fbm::<Perlin>::new(r);
+    let scale = 0.001; 
+    // Larger scale produced more 'grainy' noise, smaller scale produces clouds
+
+    let mut t = 0.0;
+
+    let mut texture_data = vec![0u8; (width * height * 4.0) as usize];
+
+    loop {
+        clear_background(BLACK);
+
+
+        for y in 0..height as usize {
+            for x in 0..width as usize {
+                let noise_value = noise.get([(x as f64 * scale), (y as f64 * scale), t]);
+                let color_value = (noise_value * 0.5 + 0.5) as f32;
+
+                let index: usize = (y * width as usize + x) * 4;
+
+                texture_data[index]     = (color_value * 255.0) as u8; // Red
+                texture_data[index + 1] = (color_value * 255.0) as u8; // Green
+                texture_data[index + 2] = (color_value * 255.0) as u8; // Blue
+                texture_data[index + 3] = 255; // Alpha
+            }
+        }
+
+        let texture = Texture2D::from_rgba8(width as u16, height as u16, &texture_data);
+
+        draw_texture(&texture, 0.0, 0.0, WHITE);
+
+        t += 0.01;
+
+        draw_fps();
+
+        next_frame().await;
+    }
+}
