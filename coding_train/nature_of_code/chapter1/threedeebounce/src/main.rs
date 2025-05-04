38fd2acccff99082bce91fb1c19f51a059f25980
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-05-04 00:16:30 -0400

    Added moving ball.

--- coding_train/nature_of_code/chapter1/threedeebounce/src/main.rs
+++ coding_train/nature_of_code/chapter1/threedeebounce/src/main.rs
@@ -4,28 +4,50 @@ use macroquad::prelude::*;
 
 #[macroquad::main("3D Bounce")]
 async fn main() {
+    const CENTER: Vec3 = Vec3::new(0., 0., 0.);
+
+    let mut ball = Ball::new(CENTER, Vec3::splat(0.01));
+
     loop{
         clear_background(LIGHTGRAY);
 
         set_camera(&Camera3D {
-            position: vec3(-20., 15., 0.0),
+            position: vec3(-25., 15., 20.0),
             up: vec3(0., 1., 0.),
-            target: vec3(0., 0., 0.),
+            target: CENTER,
             ..Default::default()
         });
 
-        // This is not visible
-        draw_cube_wires(vec3(screen_width() / 2., screen_height() / 2., 6.), vec3(2., 2., 2.), WHITE);
+        draw_cube_wires(CENTER, vec3(15., 15., 15.), PURPLE);
 
-        // These are visible
-        draw_cube_wires(vec3(0., 1., -6.), vec3(2., 2., 2.), DARKGREEN);
-        draw_cube_wires(vec3(0., 1., 6.), vec3(2., 2., 2.), DARKBLUE);
-        draw_cube_wires(vec3(2., 1., 2.), vec3(2., 2., 2.), YELLOW);
+        ball.update();
+        ball.show();
 
-        draw_cube(vec3(2., 0., -2.), vec3(0.4, 0.4, 0.4), None, BLACK);
-        
         set_default_camera();
 
         next_frame().await;
+    }
+}
+
+struct Ball {
+    pos: Vec3,
+    vel: Vec3,
+    r: f32,
+    color: Color,
+}
+
+impl Ball {
+    fn new( pos: Vec3, vel: Vec3) -> Ball {
+        let r: f32 = 1.;
+        let color = BLACK;
+
+        Ball { pos, vel, r, color }
+    }
+    fn update(&mut self) {
+        self.pos += self.vel;
+    }
+
+    fn show(&self) {
+        draw_sphere(self.pos, self.r, None, self.color);
     }
 }
