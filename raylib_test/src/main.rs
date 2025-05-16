a119de546b89cb638c1e06c5021ea56b4497bb0d
Aaron Chavez <nofoxtugiv@gmail.com>
2025-05-15 22:06:41 -0400

    Simple state machine bones for raylib test.

--- raylib_test/src/main.rs
+++ raylib_test/src/main.rs
@@ -1,4 +1,12 @@
+#![allow(unused)]
+
 use raylib::prelude::*;
+
+enum SketchState {
+    Splash,
+    Sketch,
+    Ending,
+}
 
 fn main() {
     let screen_width = 800;
@@ -6,20 +14,73 @@ fn main() {
 
     let (mut rl, thread) = raylib::init()
         .size(screen_width, screen_height)
-        .title("NoFoxTuGiv")
+        .title("RayLib Test")
         .build();
 
+    rl.set_target_fps(60);
+    let mut frame_count = 0;
+
+    let mut current_state = SketchState::Splash;
+
     while !rl.window_should_close() {
+
+
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
-        let font_size = 50;
-        let text = "NoFoxTuGiv";
-        let text_width = d.measure_text(text, font_size);
-        let x = (screen_width - text_width) / 2;
-        let y = (screen_height - font_size) / 2;
+        match current_state {
+            SketchState::Splash => {
+                draw_nofox(&mut d, &screen_width, &screen_height, &frame_count);
+                d.draw_text(&frame_count.to_string(), 50, 50, 50, Color::RAYWHITE);
+                frame_count += 1;
+                if frame_count > 150 { current_state = SketchState::Sketch }
+            },
+            SketchState::Sketch=> {
+            },
+            SketchState::Ending=> {},
+        }
 
-        d.draw_text(text, x, y, font_size, Color::WHITE);
     }
 }
 
+struct Ball {
+    pos: Vector2,
+    vel: Vector2,
+    r: i32,
+}
+
+impl Ball {
+    fn update() {
+
+    }
+
+    fn draw() {
+
+    }
+}
+
+fn handle_splash(d: &mut RaylibDrawHandle, sw: &i32, sh: &i32, fc: &mut i32, cur_state: SketchState) -> SketchState {
+    draw_nofox(d, sw, sh, fc);
+    d.draw_text(&fc.to_string(), 50, 50, 50, Color::RAYWHITE);
+    *fc += 1;
+    if *fc > 150 {
+        SketchState::Sketch
+    } else {
+        SketchState::Splash
+    }
+}
+
+fn handle_sketch(d: &mut RaylibDrawHandle, sw: &i32, sh: &i32, fc: &mut i32, cur_state: SketchState) -> SketchState {
+    // Let's start with a bouncing ball.
+    SketchState::Sketch
+}
+
+fn draw_nofox(d: &mut RaylibDrawHandle, sw: &i32, sh: &i32, fc: &i32) {
+    let font_size = 50;
+    let text = "NoFoxTuGiv";
+    let text_width = d.measure_text(text, font_size);
+    let x = (sw - text_width) / 2;
+    let y = (sh - font_size) / 2;
+
+    d.draw_text(text, x, y, font_size, Color::RAYWHITE);
+}
