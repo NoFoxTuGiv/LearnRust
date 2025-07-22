11af61c902855fdb856f316d892d090f0e4951ac
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-07-22 13:24:57 -0400

    v1.0 of Vehicles and Steering complete.
    Exercises 5.1, 5.2, 5.4, and 5.5.

--- nature_of_code/chapter5/VehiclesAndSteering/src/main.rs
+++ nature_of_code/chapter5/VehiclesAndSteering/src/main.rs
@@ -1,68 +1,118 @@
+#![allow(unused)]
+
+//! A steering behaviors simulation using the `macroquad` graphics library.
+//!
+//! This program demonstrates a single `Vehicle` agent that can perform
+//! several steering behaviors:
+//! - **Seek**: Move towards the mouse cursor.
+//! - **Flee**: Move away from the mouse cursor.
+//! - **Boundary Avoidance**: Stay within the screen bounds.
+//!
+//! Use the keyboard to toggle behaviors and debug visuals.
+//!
+//! ## Controls
+//! - `S`: Toggle seek behavior.
+//! - `F`: Toggle flee behavior.
+//! - `D`: Toggle debug view (shows forces and state).
+//! - `Esc`: Exit the application.
+
+// TODO:
+// * Update to simple state machine for force calculation.
+
 mod vehicle;
+
+use std::fmt;
 
 use macroquad::prelude::*;
 use vehicle::Vehicle;
 
+/// The main entry point for the application.
+///
+/// Initializes the simulation, creates a `Vehicle`, and enters the main game loop.
+/// The loop handles user input, calculates forces, updates the vehicle's state,
+/// and draws everything to the screen on each frame.
 #[macroquad::main(window_conf)]
 async fn main() {
+    // Seed the random number generator for vehicle placement.
     rand::srand(miniquad::date::now() as u64);
 
+    // Initialize the simulation state.
     let mut vehicle: Vehicle = Vehicle::new();
     let mut debug: bool = false;
-    let mut seeking: bool = false;
-    let mut fleeing: bool = false;
+    let mut state = State::Wander;
 
+    let mut target = Vec2::ZERO;
     let mut seek_force = Vec2::ZERO;
     let mut flee_force = Vec2::ZERO;
-    let mut target = Vec2::ZERO;
 
+    // Main loop.
     loop {
         clear_background(DARKGRAY);
 
-        // --- Target Shit ---
-        if seeking {
-            target = Vec2::new(mouse_position().0, mouse_position().1);
-            seek_force = vehicle.seek(&target);
-        } else if fleeing {
-            let threat = Vec2::new(mouse_position().0, mouse_position().1);
-            flee_force = vehicle.flee(&threat);
-        } else {
-            target = Vec2::ZERO;
-            seek_force = Vec2::ZERO;
-            flee_force = Vec2::ZERO;
+        // --- Force Calulation ---
+        // Calculate steering force based on the current mode.
+        match state {
+            State::Seek => {
+                target = Vec2::new(mouse_position().0, mouse_position().1);
+                seek_force = vehicle.seek(&target);
+                vehicle.apply_force(seek_force);
+            }
+            State::Flee => {
+                target = Vec2::new(mouse_position().0, mouse_position().1);
+                flee_force = vehicle.flee(&target, vehicle.get_flee_radius());
+                vehicle.apply_force(flee_force);
+            }
+            State::Wander => {
+                flee_force = Vec2::ZERO;
+                seek_force = Vec2::ZERO;
+                target = Vec2::ZERO - Vec2::splat(10.);
+                vehicle.wander();
+            }
         }
-        let boundary_force = vehicle.boundary_force(45.);
-        vehicle.apply_force(seek_force + boundary_force + flee_force);
+
+        // Always calculate the boundary avoidance force to keep the vehicle on screen.
+        let boundary_force = vehicle.boundary_force(100.);
+        vehicle.apply_force(boundary_force);
 
         // --- Vehicle Shit ---
         vehicle.update();
 
-        // --- Debug Drawing ---
+        // --- Debug and UI ---
         if debug {
-            draw_text("DEBUG".into(), 10., 20., 24., GRAY);
-            let seek_text = format!("Seeking: {}", seeking);
-            let flee_text = format!("Fleeing: {}", fleeing);
+            draw_text("DEBUG", screen_width() - 65., 20., 24., BLUE);
 
-            draw_text(&seek_text, 10., 50., 24., GRAY);
-            draw_text(&flee_text, 10., 80., 24., GRAY);
+            let v_pos = vehicle.get_position();
+            let scale = 5000.;
 
             // Draw Target
-            let v_pos = vehicle.get_position();
-            let scale = 500.;
-
             draw_ellipse(target.x, target.y, 10., 10., 0., GRAY);
 
-            // Draw Seek Vector
-            draw_line(
-                v_pos.x,
-                v_pos.y,
-                v_pos.x + seek_force.x * scale,
-                v_pos.y + seek_force.y * scale,
-                2.,
-                GREEN,
-            );
+            // Draw Seek/Flee vector
+            match state {
+                State::Seek => {
+                    draw_line(
+                        v_pos.x,
+                        v_pos.y,
+                        v_pos.x + seek_force.x * scale,
+                        v_pos.y + seek_force.y * scale,
+                        2.,
+                        GREEN,
+                    );
+                }
+                State::Flee => {
+                    draw_line(
+                        v_pos.x,
+                        v_pos.y,
+                        v_pos.x + flee_force.x * scale,
+                        v_pos.y + flee_force.y * scale,
+                        2.,
+                        RED,
+                    );
+                }
+                _ => {}
+            }
 
-            // Draw boundary vectors
+            // Draw boundary vectors in blue
             draw_line(
                 v_pos.x,
                 v_pos.y,
@@ -71,18 +121,14 @@ async fn main() {
                 2.,
                 BLUE,
             );
-        } else {
-            draw_text("Press D for Debug".into(), 10., 20., 24., GRAY);
-            draw_text("Press S for Seeking".into(), 10., 50., 24., GRAY);
-            draw_text("Press F for Fleeing".into(), 10., 80., 24., GRAY);
         }
-        draw_text(
-            "Press Esc to Exit.".into(),
-            10.,
-            screen_height() - 30.,
-            24.,
-            GRAY,
-        );
+
+        let state_str = format!("Current State: {}", state);
+        draw_text(&state_str, 10., screen_height() - 60., 24., GRAY);
+        draw_text("Press Esc to Exit.", 10., screen_height() - 30., 24., GRAY);
+        draw_text("Press D for Debug", 10., 20., 24., GRAY);
+        draw_text("Press S for Seeking", 10., 50., 24., GRAY);
+        draw_text("Press F for Fleeing", 10., 80., 24., GRAY);
 
         // Vehicle Drawing
         vehicle.show();
@@ -94,25 +140,53 @@ async fn main() {
 
         if is_key_pressed(KeyCode::D) {
             debug = !debug;
+            vehicle.toggle_debug();
         }
 
         if is_key_pressed(KeyCode::S) {
-            seeking = !seeking;
-            fleeing = false;
+            if state != State::Seek {
+                state = State::Seek;
+            } else {
+                state = State::Wander;
+            }
         }
 
         if is_key_pressed(KeyCode::F) {
-            fleeing = !fleeing;
-            seeking = false;
+            if state != State::Flee {
+                state = State::Flee;
+            } else {
+                state = State::Wander;
+            }
         }
 
         next_frame().await;
     }
 }
 
+/// Provides the initial configuration for the application window.
+///
+/// # Returns
+/// A `Conf` struct with custom settings.
 fn window_conf() -> Conf {
     Conf {
         window_title: "Vehicles and Steering by NoFoxTuGiv".into(),
         ..Default::default()
+    }
+}
+
+#[derive(PartialEq)]
+enum State {
+    Wander,
+    Flee,
+    Seek,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            State::Wander => write!(f, "Wandering"),
+            State::Flee => write!(f, "Fleeing"),
+            State::Seek => write!(f, "Seeking"),
+        }
     }
 }
--- nature_of_code/chapter5/VehiclesAndSteering/src/vehicle.rs
+++ nature_of_code/chapter5/VehiclesAndSteering/src/vehicle.rs
@@ -1,29 +1,59 @@
 #![allow(unused)]
 
-use macroquad::prelude::*;
+use std::f32::consts::{PI, TAU};
 
+use macroquad::{prelude::*, rand::gen_range};
+
+/// Represents an autonomous agent (a "vehicle") that can navigate its environment
+/// using steering behaviors.
 pub struct Vehicle {
+    /// Current location
     position: Vec2,
+    /// Speed and direction.
     velocity: Vec2,
+    /// Current rate of change in velocity. Reset every frame via update()
     acceleration: Vec2,
+    /// Height of the triangle used to draw the vehicle.
     height: f32,
+    /// Width of the base of the triangle
     base: f32,
+    /// Current orientation in radians.
     rotation: f32,
+    /// Max magnitude of the velocity vector
     max_speed: f32,
+    /// Max magnitude of any steering force applied
     max_force: f32,
+    /// List of target locations the vehicle may be attracted to
     targets: Vec<Vec2>,
+    /// List of threat locationst the vehicle may be repulsed by
     threats: Vec<Vec2>,
+    /// Flee radius for this vehicle
+    flee_radius: f32,
+    /// Wander direction
+    wander_theta: f32,
+    /// New wander direction cooldown
+    wander_cd: f32,
+    /// Toggles debug information for vehicle movement
+    debug: bool,
 }
 
 impl Vehicle {
     // --- NUSA (New, Update, Show, Apply_Force) ---
+
+    /// Creates a new 'Vehicle' with a random position and initial velocity.
+    ///
+    /// The vehicle is initialized with default parameters for its size, speed, and force limits
+    ///
+    /// # Returns
+    /// A new 'Vehicle' instance.
     pub fn new() -> Vehicle {
         let position = Vec2::new(
             rand::gen_range(25., screen_height() - 25.),
             rand::gen_range(25., screen_width() - 25.),
         );
-        let velocity = Vec2::new(rand::gen_range(-5., 5.), rand::gen_range(-5., 5.));
+        let velocity = Vec2::new(rand::gen_range(-3., 3.), rand::gen_range(-3., 3.));
         let acceleration = Vec2::splat(0.);
+        let wander_theta = gen_range(0., TAU);
 
         Vehicle {
             position,
@@ -32,24 +62,37 @@ impl Vehicle {
             height: 25.,
             base: 22.,
             rotation: 0.,
-            max_speed: 4.,
-            max_force: 0.1,
+            max_speed: 0.666,
+            max_force: 0.006,
             targets: vec![],
             threats: vec![],
+            flee_radius: 250.,
+            wander_theta,
+            wander_cd: 0.,
+            debug: false,
         }
     }
 
+    /// Updats the vehicle's state for the current frame.
+    ///
+    /// This method applies physics:
+    /// 1. Rotates the vehicle to face its direction of travel.
+    /// 2. Applies friction to the velocity.
+    /// 3. Updates the velocity with acceleration created via `apply_force()`
+    /// 4. Updates the position with velocity.
+    /// 5. Resets acceleration for the next frame.
     pub fn update(&mut self) {
         self.rotation = self.velocity.y.atan2(self.velocity.x) + std::f32::consts::FRAC_PI_2;
-        // self.wrap_screen(); // Eventually ditch this, assuming the vehicle will always prefer to return to the screen.
 
         self.apply_friction();
         self.velocity += self.acceleration;
+        self.velocity = self.velocity.clamp_length_max(self.max_speed);
         self.position += self.velocity;
 
         self.acceleration *= 0.;
     }
 
+    /// Renders the vehicle as a triangle on the screen.
     pub fn show(&self) {
         let v1 = Vec2::new(
             self.position.x + self.rotation.sin() * self.height / 2.,
@@ -73,26 +116,122 @@ impl Vehicle {
         draw_triangle_lines(v1, v2, v3, 2., WHITE);
     }
 
+    /// Adds force vector to vehicle's acceleration.
+    ///
+    /// Forces accumulate over a frame and are applied during the `update()` call.
+    ///
+    /// # Arguments
+    /// * `force` - The `Vec2` force to apply.
     pub fn apply_force(&mut self, force: Vec2) {
-        self.acceleration += force;
+        self.acceleration += force.clamp_length_max(self.max_force);
     }
 
     // --- Getters ---
+
+    /// Returns the current position of the vehicle.
+    ///
+    /// # Returns
+    /// A `Vec2` representing the vehicle's position.
     pub fn get_position(&self) -> Vec2 {
         self.position
     }
 
+    pub fn get_flee_radius(&self) -> f32 {
+        self.flee_radius
+    }
+
+    // --- Setters ---
+
+    /// Toggles debug boolean for the vehicle.
+    pub fn toggle_debug(&mut self) {
+        self.debug = !self.debug;
+    }
+
     // --- Behaviors ---
+
+    /// Calculates a steering force to move towards a target position.
+    ///
+    /// This behavior includes an "arrival" component, causing the vehicle to slow
+    /// as it approaches the target.
+    ///
+    /// This Vector should usually be applied via a call to `apply_force()`
+    ///
+    /// # Arguments
+    /// * `target` - A reference to the `Vec2` target position.
+    ///
+    /// # Returns
+    /// A `Vec2` steering force vector, clamped to `max_force`.
     pub fn seek(&mut self, target: &Vec2) -> Vec2 {
-        let desired = self.get_desired_velocity(target, true);
+        let desired = self.get_desired_velocity(target, true, self.max_speed);
         (desired - self.velocity).clamp_length_max(self.max_force)
     }
 
-    pub fn flee(&mut self, threat: &Vec2) -> Vec2 {
-        let desired = -self.get_desired_velocity(threat, false);
-        (desired - self.velocity).clamp_length_max(self.max_force)
+    /// Calculates a steering force to move away from a threat position.
+    ///
+    /// This Vector should usually be applied via a call to `apply_force()`
+    ///
+    /// # Arguments
+    /// * `threat` - A reference to the `Vec2` threat position.
+    /// * `flee_radius` - The distance within which the vehicle will begin to flee
+    ///   from the threat
+    ///
+    /// # Returns
+    /// A `Vec2` steering force vector, clamped to `max_force`.
+    pub fn flee(&mut self, threat: &Vec2, flee_radius: f32) -> Vec2 {
+        let distance = self.position.distance(*threat);
+
+        if distance < flee_radius {
+            let desired = -self.get_desired_velocity(threat, false, self.max_speed);
+            (desired - self.velocity).clamp_length_max(self.max_force)
+        } else {
+            self.wander(); // Wander if threat not in range
+            Vec2::ZERO
+        }
     }
 
+    pub fn wander(&mut self) {
+        let vel = self.velocity.normalize_or_zero();
+        let (cx, cy) = (
+            vel.x * 100. + self.position.x,
+            vel.y * 100. + self.position.y,
+        );
+        let r = 25.;
+        let theta = self.wander_theta;
+        let x = cx + r * f32::cos(theta);
+        let y = cy + r * f32::sin(theta);
+        let wander_vec = Vec2::new(x, y);
+
+        if self.wander_cd > 0.5 {
+            self.wander_theta = gen_range(0., TAU);
+            self.wander_cd = 0.;
+        }
+
+        let mut wander = self.get_desired_velocity(&wander_vec, false, self.max_speed * 0.50);
+        wander = (wander - self.velocity).clamp_length_max(self.max_force * 0.66);
+
+        self.apply_force(wander);
+
+        self.wander_cd += get_frame_time();
+
+        // Debug drawing
+        if self.debug {
+            draw_ellipse_lines(cx, cy, r, r, theta, 2., WHITE);
+            draw_line(cx, cy, x, y, 2., WHITE);
+        }
+    }
+
+    /// Calculates a steering force to keep the vehicle within the screen boundaries.
+    ///
+    /// The force increases proportionally to the vehicle's distance into the margin.
+    ///
+    /// This Vector should usually be applied via a call to `apply_force()`
+    ///
+    /// # Arguments
+    /// * `margin` - The distance from the screen edge where the boundary force starts
+    ///
+    /// # Returns
+    /// A `Vec2` steering force vector if the vehicle is within the margin, otherwise,
+    /// `Vec2::ZERO`
     pub fn boundary_force(&mut self, margin: f32) -> Vec2 {
         let mut force = Vec2::ZERO;
 
@@ -116,32 +255,50 @@ impl Vehicle {
 
         if force != Vec2::ZERO {
             let desired = force.normalize_or_zero() * self.max_speed;
-            return (desired - self.velocity).clamp_length_max(self.max_force);
+            return (desired - self.velocity).clamp_length_max(self.max_force * 1.33);
         }
 
         Vec2::ZERO
     }
 
     // --- Private Fns ---
-    fn get_desired_velocity(&mut self, target: &Vec2, arrive: bool) -> Vec2 {
+
+    /// A helper function to calculate the desired velocity towards a target.
+    ///
+    /// If `arrive` is true, the speed is scaled down as the vehicle approaches the target.
+    ///
+    /// # Arguments
+    /// * `target` - The position to move towards
+    /// * `arrive` - A boolean indicating whether to enable arrival behavior.
+    ///
+    /// # Returns
+    /// A `Vec2` representing the desired velocity.
+    fn get_desired_velocity(&mut self, target: &Vec2, arrive: bool, max_speed: f32) -> Vec2 {
         let mut desired: Vec2 = *target - self.position;
         let distance: f32 = desired.length();
         let speed: f32 = if arrive && distance < 100.0 {
             map_range(distance, 0., 100., 0., self.max_speed)
         } else {
-            self.max_speed
+            max_speed
         };
         desired.normalize_or_zero() * speed
     }
 
+    /// Applies a small amount of friction to the vehicle's velocity every frame.
     fn apply_friction(&mut self) {
         self.velocity *= 0.999;
     }
 
-    fn wrap_screen(&mut self) {
+    /// Teleports the vehicle to the opposite side of the screen if it goes too far out of bounds.
+    ///
+    /// # Arguments
+    /// * `overscan` - Ammount to allow the vehicle to run off-screen before teleporting the
+    ///   vehicle to the opposite side.
+    #[allow(unused)]
+    fn wrap_screen(&mut self, overscan: f32) {
         match self.position.x {
-            x if x <= 0. => self.position.x = screen_width(),
-            x if x >= screen_width() => self.position.x = 0.,
+            x if x <= 0. - overscan => self.position.x = screen_width() + overscan,
+            x if x >= screen_width() + overscan => self.position.x = 0. - overscan,
             _ => {}
         }
 
@@ -154,6 +311,29 @@ impl Vehicle {
 }
 
 // --- Helper Functions ---
-fn map_range(value: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+
+/// Remaps a number from one range to another.
+///
+/// # Arguments
+/// * `value` - The incoming value to be converted.
+/// * `in_min` - The lower bound of the value's current range.
+/// * `in_max` - The upper bound of the value's current range.
+/// * `out_min` - The lower bound of the value's target range.
+/// * `out_max` - The upper bound of the value's target range.
+///
+/// # Type Parameters
+/// * `T` - A type that supports basic arithmetic (+, -, *, /) and can be copied.
+///   (i.e. `f32`, `f64`, `i32`, `u64`, etc., etc.)
+///
+/// # Returns
+/// The remapped value as an `f32`.
+fn map_range<T>(value: T, in_min: T, in_max: T, out_min: T, out_max: T) -> T
+where
+    T: std::ops::Add<Output = T>
+        + std::ops::Sub<Output = T>
+        + std::ops::Mul<Output = T>
+        + std::ops::Div<Output = T>
+        + Copy,
+{
     out_min + (out_max - out_min) * ((value - in_min) / (in_max - in_min))
 }
