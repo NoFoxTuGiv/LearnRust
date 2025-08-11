affd7f7976059a26d4050269b95b95d984d3bb1a
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-08-10 21:24:33 -0400

    Fix angle calculation for angle between.
    Fixed angle calculation for angle between.
    Improved text formatting for display.
    Began draw_vector function to improve line drawing.

--- nature_of_code/chapter5/AngleBetweenVectors/src/main.rs
+++ nature_of_code/chapter5/AngleBetweenVectors/src/main.rs
@@ -8,13 +8,17 @@ async fn main() {
         clear_background(DARKGRAY);
 
         let scrn_center = Vec2::new(screen_width() / 2., screen_height() / 2.);
-        let x_axis = Vec2::new(scrn_center.x + 100., scrn_center.y);
+        let x_axis = Vec2::new(100., 0.);
         let mouse_pos = Vec2::new(mouse_position().0, mouse_position().1);
-        let mut v = (mouse_pos - scrn_center);
-        v = v.normalize();
-        v *= 100.;
+        let mut v = set_mag(mouse_pos - scrn_center, 100.);
 
-        draw_line(scrn_center.x, scrn_center.y, x_axis.x, x_axis.y, 5., WHITE);
+        draw_line(
+            scrn_center.x,
+            scrn_center.y,
+            scrn_center.x + x_axis.x,
+            scrn_center.y + x_axis.y,
+            5.,
+            WHITE);
         draw_line(
             scrn_center.x,
             scrn_center.y,
@@ -23,11 +27,15 @@ async fn main() {
             5.,
             WHITE,
         );
+        draw_ellipse(scrn_center.x, scrn_center.y, 5., 5., 0., WHITE);
 
         let theta = v.angle_between(x_axis);
+        let theta_text = format!("{:.2} radians", theta);
+        draw_text(&theta_text, 50., 50., 24., WHITE);
 
-        draw_text(&theta.to_string(), 50., 50., 24., WHITE);
-        draw_text(&theta.to_degrees().to_string(), 50., 100., 42., WHITE);
+        let degrees = theta.to_degrees() as i16;
+        let degrees_text = format!("°{}", degrees);
+        draw_text(&degrees_text, 50., 100., 42., WHITE);
 
         if is_key_pressed(KeyCode::Escape) {
             break;
@@ -40,11 +48,21 @@ async fn main() {
 fn window_conf() -> Conf {
     Conf {
         window_title: String::from("AngleBetweenVectors"),
+        window_width: 400,
+        window_height: 360,
+        window_resizable: false,
         ..Default::default()
     }
 }
 
+fn set_mag(vec: Vec2, scalar: f32) -> Vec2 {
+    vec.normalize() * scalar
+}
+
+/// Draws an arrow pointing from a to b
+///
+/// # Parameters
 #[allow(unused)]
-fn draw_vector(a: &Vec2, b: &Vec2) {
+fn draw_vertex(a: Vec2, b: Vec2) {
     draw_line(a.x, a.y, b.x, b.y, 5., WHITE);
 }
